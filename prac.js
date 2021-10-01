b836203465762ad259f0d7d49aa03e7bb8dbf42b
Mohammadibrahim1996 <imd25775@gmail.com>
2021-10-01 18:34:46 +0530

    some-changes

--- prac.js
+++ prac.js
@@ -10,8 +10,33 @@
 //     i++;
 // };
 
-let i = 0 ;
-while(i<=10){
-    if (i % 2 !== 0) console.log (i);
-    i++;
+// let i = 0 ;
+// while(i<=10){
+//     if (i % 2 !== 0) console.log (i);
+//     i++;
+// };
+
+//for in helps you iterate over an object targetting the keys
+//it can also iterate over an array but that is not good practise
+
+// const colors = [ "red","green","blue"];
+ 
+// for (let items in colors){
+//     console.log (colors[items]);
+// };
+
+// const person = {
+//     name: Mak" ,
+//     age: 25 ,
+// };
+
+// for (let key in person ){
+//     console.log (key,person[key]);
+// };
+
+//for of type
+
+const numbers = [10,20,30,40];
+for (let anything of numbers ){
+    console.log(anything+5);
 };
