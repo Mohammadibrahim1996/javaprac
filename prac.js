8220d8f99e923d559e039682611eac52a3ded325
Mohammadibrahim1996 <imd25775@gmail.com>
2021-10-01 18:01:11 +0530

    java-prac

---
+++ prac.js
@@ -0,0 +1,17 @@
+
+ 
+// for (let i=0; i <= 5 ; i++) {
+//     console.log("hello World",i);
+// };
+
+// let i = 0 ;
+// while(i<=10){
+//     if (i % 2 === 0) console.log (i);
+//     i++;
+// };
+
+let i = 0 ;
+while(i<=10){
+    if (i % 2 !== 0) console.log (i);
+    i++;
+};
