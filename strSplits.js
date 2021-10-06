a1b845e8ed1696eb9a6589ce45b7ef698ed1c39a
Mohammadibrahim1996 <imd25775@gmail.com>
2021-10-06 12:57:52 +0530

    str-Splits

---
+++ strSplits.js
@@ -0,0 +1,4 @@
+const str = " Automatically on, automatically connecte Easy setup for all your Apple devices Quick access to Siri by saying Hey Siri Double-tap to play or skip forward Automatically on, automatically connected Easy setup for all your Apple devicesQuick access to Siri by saying “Hey Siri” Double-tap to play or skip forward"
+ const arrstr = `${str.split(' ').slice(0,25).join(' ')}...`;
+
+ console.log(arrstr);
