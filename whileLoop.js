f2e14cf9593ea73124997718e9076277abe82540
Mohammadibrahim1996 <imd25775@gmail.com>
2021-10-06 23:19:19 +0530

    do-while-loop

--- whileLoop.js
+++ whileLoop.js
@@ -1,10 +1,15 @@
-// for (i=5;i>=0;i--){
+//while Loop
 
-//     if (i%2 !==0) console.log(i);
+// let i=0;
+// while(i<=5){
+//     if (i%2 !==0); console.log(i);
+//     i++;
 // };
 
-let i=0;
-while(i<=5){
+
+//do-while Loop
+
+let i=9;
+do{
     if (i%2 !==0); console.log(i);
-    i++;
-};
+}while(i<=5);
