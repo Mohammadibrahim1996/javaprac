3e72bb30570af8d81e215f450e2c28408df05bbc
Mohammadibrahim1996 <imd25775@gmail.com>
2021-10-08 15:13:59 +0530

    is-number-Prime

---
+++ m.js
@@ -0,0 +1,20 @@
+//AS SIMPLE AS POSSIBLE
+
+// function isPrime(num) {
+//     for(var i = 2; i < num; i++)
+//       if(num % i === 0) return false;
+//     return num > 1;
+//   };
+
+//   console.log(isPrime(10));
+
+  ///with es6 syntax
+
+
+  const isPrime = num => {
+    for(let i = 2; i < num; i++)
+      if(num % i === 0) return false;
+    return num > 1;
+  };
+
+  console.log(isPrime(10));
