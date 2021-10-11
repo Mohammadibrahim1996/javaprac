3a4afd0f438fce9e39df2f5351536f156b783cea
Mohammadibrahim1996 <imd25775@gmail.com>
2021-10-12 02:19:15 +0530

    change

--- fizbuzz.js
+++ fizbuzz.js
@@ -4,27 +4,6 @@
 //not divisble by 3 or 5 => input 
 // not a number => Nan
 
-// const output = fizzBuzz(3);
-
-// function fizzBuzz(input) {
-    
-//     if ((input % 3 === 0) && (input % 5 === 0))
-//     return 'FizzBuzz';
-    
-//     if (typeof input !== "number")
-//     return NaN;
-    
-//     if (input % 3 === 0)
-//     return 'Fizz';
-    
-//     if (input % 5 === 0 )
-//     return 'Buzz';
-    
-//     return input;
-// };
-
-// console.log (output);
-
 const output = fizzBuzz(3);
 
 function fizzBuzz(input) {
--- landscape.js
+++ landscape.js
@@ -1,3 +1,9 @@
+// console.log (islandscape(300,500));
+
+// function islandscape(width,height) {
+//    return (width > height );
+// };
+
 console.log (islandscape(300,500));
 
 function islandscape(width,height) {
