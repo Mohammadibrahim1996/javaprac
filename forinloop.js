5d20c3010d262dff5a859de0cf44c7a912fd3634
Mohammadibrahim1996 <imd25775@gmail.com>
2021-10-12 02:22:36 +0530

    changes

--- forinloop.js
+++ forinloop.js
@@ -1,18 +1,36 @@
+// //for in
+//  const person = {
+
+//      name : "mosh" ,
+//      age : 30 
+//  };
+
+//  for (let key in person)
+//  console.log (key,person[key]);
+
+//  const colors =['red' ,'blue' , 'green'];
+//  for (let index in colors)
+//  console.log(index, colors[index]);
+
+//  //for-of
+//  for (let color of colors)
+//  console.log(color);
+
 //for in
- const person = {
+const person = {
 
-     name : "mosh" ,
-     age : 30 
- };
+    name : "mosh" ,
+    age : 30 
+};
 
- for (let key in person)
- console.log (key,person[key]);
+for (let key in person)
+console.log (key,person[key]);
 
- const colors =['red' ,'blue' , 'green'];
- for (let index in colors)
- console.log(index, colors[index]);
+const colors =['red' ,'blue' , 'green'];
+for (let index in colors)
+console.log(index, colors[index]);
 
- //for-of
- for (let color of colors)
- console.log(color);
+//for-of
+for (let color of colors)
+console.log(color);
 
