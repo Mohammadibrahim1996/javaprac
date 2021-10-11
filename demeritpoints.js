b678f74e2b7d2491d9a33f28d34086fbb5f6f26a
Mohammadibrahim1996 <imd25775@gmail.com>
2021-10-12 02:17:12 +0530

    excersie

--- demeritpoints.js
+++ demeritpoints.js
@@ -3,6 +3,26 @@
 //math.floor(1.30
 //12 points -> suspend
 
+// checkspeed (130);
+
+// function checkspeed(speed){
+//     const speedlimit =70;
+//     const kmperpoint= 5;
+    
+//     if (speed < speedlimit+kmperpoint){ 
+
+//         console.log ("Ok");
+//         return;
+//     }
+//  else {
+//     const points = Math.floor ( (speed - speedlimit)/ kmperpoint);
+//     if (points >= 12)
+//     console.log("LICENSED SUSPENDED");
+//     else
+//      console.log("points", points);
+//     }
+// }
+
 checkspeed (130);
 
 function checkspeed(speed){
