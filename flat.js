81a3ea812ee5e984294791c035e4cfe82305ca4f
Mohammadibrahim1996 <imd25775@gmail.com>
2021-10-13 00:04:15 +0530

    interview

---
+++ flat.js
@@ -0,0 +1,17 @@
+// number = [ 1,2,3,4,[[5,6],7]];
+
+// console.log (number.flat(2));
+
+
+// String
+ const str = "rashmi";
+ const strArr = str.split('');
+//  const revArr = strArr.reverse();
+//  const revStr = revArr.join('');
+//  console.log (revStr);
+ const newArr =[];
+ for (let i=0; i<0 ;i--){
+     newArr.push(strArr(i));
+return revArr = newArr.join('');
+    };
+ console.log (newArr);
