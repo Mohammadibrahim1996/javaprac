baa086ef1c6fc544e60ca5185f8c4fea3f75933b
Mohammadibrahim1996 <imd25775@gmail.com>
2021-10-13 00:19:21 +0530

    OOP

---
+++ objects.js
@@ -0,0 +1,14 @@
+//object-oroented programming (OOP)
+const Circle ={
+    radius:1,
+    location :{
+        x:1,
+        y:1
+    },
+    isvisible:true,
+    draw: function() {
+        console.log("draw");
+    }
+};
+
+Circle.draw(); //Method
