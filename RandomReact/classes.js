51115932b54fd859223d05b73b16d17b0cf75fd2
Mohammadibrahim1996 <imd25775@gmail.com>
2021-10-20 16:02:14 +0530

    added

--- RandomReact/classes.js
+++ RandomReact/classes.js
@@ -14,17 +14,33 @@ class Person {
 // console.log(details);
 // console.log(details.greetings());
 
-///subClass
- class Contact extends Person {
-     constructor (fname,lname,phone,city){
-         super(fname,lname);
-         this.number=phone;
-         this.shaher=city;
-     }
-     getContact (){
-        return `Contact: ${this.number} city:${this.shaher}`;
-     }
- }
-    const deatils = new Contact("jhon","wick",8983134798,"newYork")
+// ///subClass
+//  class Contact extends Person {
+//      constructor (fname,lname,phone,city){
+//          super(fname,lname);
+//          this.number=phone;
+//          this.shaher=city;
+//      }
+//      getContact (){
+//         return `Contact: ${this.number} city:${this.shaher}`;
+//      }
+//  }
+//     const deatils = new Contact("jhon","wick",8983134798,"newYork")
+
+// console.log(deatils.greetings(),deatils.getContact());
+
+
+
+class Contact extends Person {
+    constructor (fname,lname,phone,city){
+        super(fname,lname);
+        this.number=phone;
+        this.shaher=city;
+    }
+    getContact (){
+       return `Contact: ${this.number} city:${this.shaher}`;
+    }
+}
+   const deatils = new Contact("jhon","wick",8983134798,"newYork")
 
 console.log(deatils.greetings(),deatils.getContact());
