dae53faba2dc7ff7ed7a633ac2a73a8764c3cca6
Mohammadibrahim1996 <imd25775@gmail.com>
2021-10-20 16:01:08 +0530

    class

---
+++ RandomReact/classes.js
@@ -0,0 +1,30 @@
+
+//parentClass
+class Person {
+    constructor(fname,lname){
+        this.FirstName= fname;
+        this.LastName = lname;
+    }
+    greetings(){
+        return`Welcome ${this.FirstName}${this.LastName}`;
+    }
+}
+
+// const details = new Person("jhon","Wick");
+// console.log(details);
+// console.log(details.greetings());
+
+///subClass
+ class Contact extends Person {
+     constructor (fname,lname,phone,city){
+         super(fname,lname);
+         this.number=phone;
+         this.shaher=city;
+     }
+     getContact (){
+        return `Contact: ${this.number} city:${this.shaher}`;
+     }
+ }
+    const deatils = new Contact("jhon","wick",8983134798,"newYork")
+
+console.log(deatils.greetings(),deatils.getContact());
