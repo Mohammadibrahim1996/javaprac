6e836f42596d6921e2abc94c732f2bf9fa334c69
Mohammadibrahim1996 <imd25775@gmail.com>
2021-10-23 12:41:00 +0530

    added

--- prac.js
+++ prac.js
@@ -1,59 +1,68 @@
 
  
-// for (let i=0; i <= 5 ; i++) {
-//     console.log("hello World",i);
-// };
+for (let i=0; i <= 5 ; i++) {
+    console.log("hello World",i);
+};
 
-// let i = 0 ;
-// while(i<=10){
-//     if (i % 2 === 0) console.log (i);
-//     i++;
-// };
+let i = 0 ;
+while(i<=10){
+    if (i % 2 === 0) console.log (i);
+    i++;
+};
 
-// let i = 0 ;
-// while(i<=10){
-//     if (i % 2 !== 0) console.log (i);
-//     i++;
-// };
+let i = 0 ;
+while(i<=10){
+    if (i % 2 !== 0) console.log (i);
+    i++;
+};
 
-//for in helps you iterate over an object targetting the keys
-//it can also iterate over an array but that is not good practise
+for in helps you iterate over an object targetting the keys
+it can also iterate over an array but that is not good practise
 
-// const colors = [ "red","green","blue"];
+const colors = [ "red","green","blue"];
  
-// for (let items in colors){
-//     console.log (colors[items]);
-// };
+for (let items in colors){
+    console.log (colors[items]);
+};
 
-// const person = {
-//     name: Mak" ,
-//     age: 25 ,
-// };
+const person = {
+    name: Mak" ,
+    age: 25 ,
+};
 
-// for (let key in person ){
-//     console.log (key,person[key]);
-// };
+for (let key in person ){
+    console.log (key,person[key]);
+};
 
-//for of type
+for of type
 
-// const numbers = [10,20,30,40];
-// for (let anything of numbers ){
-//     console.log(anything+5);
-// };
+const numbers = [10,20,30,40];
+for (let anything of numbers ){
+    console.log(anything+5);
+};
 
-// console.log("Hello World")
+console.log("Hello World")
  
-// let a='red';
-// let b='blue';
+let a='red';
+let b='blue';
 
-// let c=a;
-//  a=b;
-//  b=c;
-// console.log(a);
-// console.log(b);
+let c=a;
+ a=b;
+ b=c;
+console.log(a);
+console.log(b);
 
 const a = function(x,y){
     return x+y;
 }
 const b = a(15, 10);
 console.log(b)
+
+function reverseString(str) {
+    if (str === "")
+      return "";
+    else
+      return reverseString(str.substr(1)) + str.charAt(0);
+  }
+  reverseString("hello");
+   console.log(reverseString());
