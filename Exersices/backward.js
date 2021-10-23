1eee7a393a7a50c61752315c999b22c2d16f3ed8
Mohammadibrahim1996 <imd25775@gmail.com>
2021-10-23 23:52:28 +0530

    changes

--- Exersices/backward.js
+++ Exersices/backward.js
@@ -1,3 +1,14 @@
+// var x = "Hello World";
+
+// var backword = x.split("").reverse().join("");
+
+// // console.log(backword);
+// var x=4
+// const test(x) =()=>{ x===5 ? true: false};
+
+// console.log(test);
+
+
 var x = "Hello World";
 
 var backword = x.split("").reverse().join("");
