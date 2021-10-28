dc13ca7854eca5807508066964e69d8000b33f2a
Mohammadibrahim1996 <imd25775@gmail.com>
2021-10-28 23:03:32 +0530

    added

--- LearnJavaScript/Basic/variable&datatypes.js
+++ LearnJavaScript/Basic/variable&datatypes.js
@@ -48,3 +48,20 @@ console.log(job);
 job = 'Teacher';
 console.log(job);
 
+
+
+// Variables and data types///
+var firstName = 'John';
+console.log(firstName);
+
+var lastName = 'Smith';
+var age = 28;
+
+var fullAge = true;
+console.log(fullAge);
+
+var job;
+console.log(job);
+
+job = 'Teacher';
+console.log(job);
