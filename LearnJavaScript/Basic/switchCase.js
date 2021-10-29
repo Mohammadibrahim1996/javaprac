d10c990c7a5e76672ca579b78e765e1b6124cb33
Mohammadibrahim1996 <imd25775@gmail.com>
2021-10-29 17:32:08 +0530

    added

---
+++ LearnJavaScript/Basic/switchCase.js
@@ -0,0 +1,25 @@
+const day = "wednesday" ;
+
+switch(day){
+    case "monday":
+        console.log("happy monday")
+        break;
+     case "tuesday":
+         console.log ( " happy tuesday")   
+         break;
+         case "wednesday":
+             case "thursday":
+                 console.log("do coding" )
+                 break;
+                 case "friday":
+                    console.log("happy friday")
+                    break;
+                    case "saturday":
+                        case "sunday":
+                            console.log (" happy weekend")
+                            break;
+                            default:
+                                console.log("not a valid day")
+                                break;
+
+};
