ef397c1fa1e07ff762ca1f1b34ceb92e65c96146
Mohammadibrahim1996 <imd25775@gmail.com>
2021-11-02 19:10:50 +0530

    factory

---
+++ OOP/jsfactory.js
@@ -0,0 +1,13 @@
+function creatPerson(fname,lname){
+    return{
+        fname: fname,
+        lname: lname,
+        getFullName(){
+            return fname + ' ' + lname;
+        }
+    }
+}
+
+const  Person = new creatPerson("sahil","Mustafa");
+console.log(Person);
+console.log(Person.getFullName());
