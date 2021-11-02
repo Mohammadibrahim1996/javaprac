30ff2939fa5133a80eb1d2e30dbbba966a2e6c0b
Mohammadibrahim1996 <imd25775@gmail.com>
2021-11-02 17:25:31 +0530

    added

---
+++ LearnJavaScript/Functions/funcWithParam.js
@@ -0,0 +1,18 @@
+var a;
+function display(a){
+    console.log(a);
+};
+display("shaktiMan");
+    
+let y;
+function show(y){
+    console.log(y);
+};
+show ("Iron Man")
+
+
+const x = "x"
+function item(x){
+    console.log(x);
+};
+item("apple")
