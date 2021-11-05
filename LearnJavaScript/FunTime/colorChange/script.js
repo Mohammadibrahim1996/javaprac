30b5699e430ef00c5cf935e288eb08f018e7e3f1
Mohammadibrahim1996 <imd25775@gmail.com>
2021-11-05 19:00:00 +0530

    class

--- LearnJavaScript/FunTime/colorChange/script.js
+++ LearnJavaScript/FunTime/colorChange/script.js
@@ -1,9 +1,13 @@
 let btnPrimary= document.querySelector("#primary");
 let btnDanger= document.querySelector("#danger");
 let btnSuccess= document.querySelector("#success");
-let pPara1=document.querySelector("para1")
+// let x= document.querySelector("#pPara1")
 
 btnPrimary.addEventListener("click",()=>btnPrimary.style.backgroundColor= "#337ab7")
 btnDanger.addEventListener("click",()=>btnDanger.style.backgroundColor= "#c9302c")
 btnSuccess.addEventListener("click",()=>btnSuccess.style.backgroundColor= "#4cae4c")
-// pPara1.addEventListener("click",()=>pPara1.style.backgroundColor="c9302c")
+function myFunction() {
+    var x = document.querySelectorAll("p.example");
+    x[0].style.backgroundColor = "red";
+  }
+  
