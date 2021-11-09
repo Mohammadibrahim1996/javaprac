3d0d646d99c6c188156368432fd73fa7aa331f9a
Mohammadibrahim1996 <imd25775@gmail.com>
2021-11-09 17:22:50 +0530

    loopinloop

---
+++ LearnJavaScript/Basic/loopinBakwardandloopsinloops.js
@@ -0,0 +1,11 @@
+const sahil =[
+    "mohammmad",
+    "ibrahim",
+    2025-1996,
+    "devloper",
+    ['khajaSir','makSir','salmanSir']
+];
+
+for (let i=sahil.length-1 ; i>=0 ; i--){
+    console.log(i,sahil[i]);
+}
