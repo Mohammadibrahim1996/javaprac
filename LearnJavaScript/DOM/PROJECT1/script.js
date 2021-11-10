b54a240f857b1f1bbd8d722a2e4f40c02c6cfd7e
Mohammadibrahim1996 <imd25775@gmail.com>
2021-11-10 18:18:38 +0530

    added

---
+++ LearnJavaScript/DOM/PROJECT1/script.js
@@ -0,0 +1,10 @@
+const number = Math.trunc(Math.random()*20)+1;
+document.querySelector(".number").textContent= number;
+
+
+document.querySelector(".check").addEventListener("click",function(){
+    const guess = Number (document.querySelector(".guess").value);
+ if (!guess){
+     document.querySelector(".message").textContent= "⛔⛔NO Number"
+ }
+});
