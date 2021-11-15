e5bb5cc1e7aab38bd9a4def0ed52341cc2510e62
Mohammadibrahim1996 <imd25775@gmail.com>
2021-11-15 23:51:19 +0530

    added

--- LearnJavaScript/DOM/PROJECT1/script.js
+++ LearnJavaScript/DOM/PROJECT1/script.js
@@ -33,3 +33,14 @@ document.querySelector(".check").addEventListener("click",function(){
      document.querySelector(".message").textContent= "⛔⛔NO Number"
  }
 });
+
+const number = Math.trunc(Math.random()*20)+1;
+document.querySelector(".number").textContent= number;
+
+
+document.querySelector(".check").addEventListener("click",function(){
+    const guess = Number (document.querySelector(".guess").value);
+ if (!guess){
+     document.querySelector(".message").textContent= "⛔⛔NO Number"
+ }
+});
