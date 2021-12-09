d219a4a4f2047fb6b05d957f83b41866048752e7
Mohammadibrahim1996 <imd25775@gmail.com>
2021-12-09 18:50:33 +0530

    added

--- LearnJavaScript/FunTime/Random/tryCatch.js
+++ LearnJavaScript/FunTime/Random/tryCatch.js
@@ -5,3 +5,11 @@ return myArry.reduce((acc,cur)=>{
     return acc[cur] ? acc[cur] += 1 : acc[cur]=1,acc;
 },{})
 }
+
+const arr = [1,2,3,1,2,3,1,2,3];
+console.log(countOcc(arr));
+function countOcc (myArry){
+return myArry.reduce((acc,cur)=>{
+    return acc[cur] ? acc[cur] += 1 : acc[cur]=1,acc;
+},{})
+}
