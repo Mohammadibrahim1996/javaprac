301ce1e0fe465b543db0a8d928ed21df07cb8c27
Mohammadibrahim1996 <imd25775@gmail.com>
2021-12-12 18:08:56 +0530

    added

--- Exersices/findthemax.js
+++ Exersices/findthemax.js
@@ -5,3 +5,20 @@ function max (a,b ) {
  return (a > b) ? a : b ;
 };
 console.log (number);
+
+
+let number = max(5,2);
+ 
+function max (a,b ) {
+ //conditional operator
+ return (a > b) ? a : b ;
+};
+console.log (number);
+
+let number = max(5,2);
+ 
+function max (a,b ) {
+ //conditional operator
+ return (a > b) ? a : b ;
+};
+console.log (number);
