2ca06fd82ae17790e20ba861958c6f3ea6bef90e
Mohammadibrahim1996 <imd25775@gmail.com>
2021-12-21 23:43:53 +0530

    added

--- LearnJavaScript/eventPropagation/eventPropagation.js
+++ LearnJavaScript/eventPropagation/eventPropagation.js
@@ -1,9 +1,11 @@
 document.querySelector("#grandparent")
 .addEventListener("click",(e)=>{console.log("GrandParent click")
-e.stopPropagation});
+e.stopPropagation()});
+
 document.querySelector("#parent")
 .addEventListener("click",(e)=>{console.log("Parent click!")
 e.stopPropagation()});
+
 document.querySelector("#child")
 .addEventListener("click",(e)=>{console.log("child Click!")
 e.stopPropagation()});
