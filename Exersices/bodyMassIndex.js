986cb3a86c67fe78fb02cf2e8e3c9ae1ab3dcba8
Mohammadibrahim1996 <imd25775@gmail.com>
2021-12-27 23:44:21 +0530

    added

--- Exersices/bodyMassIndex.js
+++ Exersices/bodyMassIndex.js
@@ -1,20 +1,23 @@
-let markMass= 95 ;
-let markHeight=1.88 ;
-let jhonMass= 85 ;
-let jhonHeight=1.76;
+let markMass = 95;
+let markHeight = 1.88;
+let jhonMass = 85;
+let jhonHeight = 1.76;
 
-bmiOfMark=markMass/markHeight**2;
-bmiOfJhon=jhonMass/(jhonHeight*jhonHeight);
+bmiOfMark = markMass / markHeight ** 2;
+bmiOfJhon = jhonMass / (jhonHeight * jhonHeight);
 
 //  markHigherBmi=(bmiOfMark>bmiOfJhon);
 
 //  console.log(bmiOfMark,bmiOfJhon,markHigherBmi);
 
-if(bmiOfMark > bmiOfJhon){
-    console.log(`marks bmi ${bmiOfMark} is higher than jhon bmi ${bmiOfJhon}`);
-} else{
-    console.log(`jhon ${bmiOfJhon} bmi is higher than marks bmi ${bmiOfMark}`);
+if (bmiOfMark > bmiOfJhon) {
+  console.log(`marks bmi ${bmiOfMark} is higher than jhon bmi ${bmiOfJhon}`);
+} else {
+  console.log(`jhon ${bmiOfJhon} bmi is higher than marks bmi ${bmiOfMark}`);
 }
 
-
-
+if (bmiOfMark > bmiOfJhon) {
+  console.log(`marks bmi ${bmiOfMark} is higher than jhon bmi ${bmiOfJhon}`);
+} else {
+  console.log(`jhon ${bmiOfJhon} bmi is higher than marks bmi ${bmiOfMark}`);
+}
