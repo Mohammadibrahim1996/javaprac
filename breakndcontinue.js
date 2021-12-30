0be74d878cf7ad9193f93f3970938ce22a0bff51
Mohammadibrahim1996 <imd25775@gmail.com>
2021-12-30 20:39:09 +0530

    added

--- breakndcontinue.js
+++ breakndcontinue.js
@@ -1,15 +1,25 @@
-
-
-let i= 0;
+let i = 0;
 while (i <= 10) {
+  // if (i === 5) break;
+  // i++;
+  if (i % 2 === 0) {
+    i++;
+    continue;
+  }
 
-    // if (i === 5) break;
-    // i++;
-    if (i % 2 ===0){ 
-        i++;
-        continue;
-    }
+  console.log(i);
+  i++;
+}
 
-    console.log(i);
-i++;
+let i = 0;
+while (i <= 10) {
+  // if (i === 5) break;
+  // i++;
+  if (i % 2 === 0) {
+    i++;
+    continue;
+  }
+
+  console.log(i);
+  i++;
 }
