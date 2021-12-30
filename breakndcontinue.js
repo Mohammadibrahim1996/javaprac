41ccce6173772e4359621a541195dabe03cbec24
Mohammadibrahim1996 <imd25775@gmail.com>
2021-12-30 20:41:13 +0530

    removed

--- breakndcontinue.js
+++ breakndcontinue.js
@@ -10,29 +10,3 @@ while (i <= 10) {
   console.log(i);
   i++;
 }
-
-let i = 0;
-while (i <= 10) {
-  // if (i === 5) break;
-  // i++;
-  if (i % 2 === 0) {
-    i++;
-    continue;
-  }
-
-  console.log(i);
-  i++;
-}
-
-let i = 0;
-while (i <= 10) {
-  // if (i === 5) break;
-  // i++;
-  if (i % 2 === 0) {
-    i++;
-    continue;
-  }
-
-  console.log(i);
-  i++;
-}
