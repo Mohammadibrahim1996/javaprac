3cc2b2b79ad50af17e4629f0f8a5cbbab90096c4
Mohammadibrahim1996 <imd25775@gmail.com>
2022-01-03 23:09:07 +0530

    b

--- forLoop.js
+++ forLoop.js
@@ -3,8 +3,10 @@
 //     if (i%2 !==0) console.log(i);
 // };
 
+for (i = 5; i >= 0; i--) {
+  if (i % 2 !== 0) console.log(i);
+}
 
-for (i=5;i>=0;i--){
-
-    if (i%2 !==0) console.log(i);
-};
+for (i = 5; i >= 0; i--) {
+  if (i % 2 !== 0) console.log(i);
+}
