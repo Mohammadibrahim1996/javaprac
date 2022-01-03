45a69f5f8c2fcb19a6e57b55363216e79af5a386
Mohammadibrahim1996 <imd25775@gmail.com>
2022-01-03 23:12:31 +0530

    delete

--- ifElse.js
+++ ifElse.js
@@ -1,9 +1,4 @@
-// let hour = 10;
-// if (hour >= 6 && hour < 12) console.log("Good Morning");
-// else if (hour >= 12 && hour < 18) console.log("Good Afternoon");
-// else console.log("Good Evening");
-
-var hour = 12;
+let hour = 10;
 if (hour >= 6 && hour < 12) console.log("Good Morning");
 else if (hour >= 12 && hour < 18) console.log("Good Afternoon");
 else console.log("Good Evening");
