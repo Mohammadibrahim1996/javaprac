1482f2aaa3b6edd38217b670d0b54d55e6020da4
Mohammadibrahim1996 <imd25775@gmail.com>
2022-01-03 23:06:32 +0530

    hhh

--- demeritpoints.js
+++ demeritpoints.js
@@ -3,23 +3,32 @@
 //math.floor(1.30
 //12 points -> suspend
 
-checkspeed (130);
+checkspeed(130);
 
-function checkspeed(speed){
-    const speedlimit =70;
-    const kmperpoint= 5;
-    
-    if (speed < speedlimit+kmperpoint){ 
+function checkspeed(speed) {
+  const speedlimit = 70;
+  const kmperpoint = 5;
 
-        console.log ("Ok");
-        return;
-    }
- else {
-    const points = Math.floor ( (speed - speedlimit)/ kmperpoint);
-    if (points >= 12)
-    console.log("LICENSED SUSPENDED");
-    else
-     console.log("points", points);
-    }
+  if (speed < speedlimit + kmperpoint) {
+    console.log("Ok");
+    return;
+  } else {
+    const points = Math.floor((speed - speedlimit) / kmperpoint);
+    if (points >= 12) console.log("LICENSED SUSPENDED");
+    else console.log("points", points);
+  }
 }
 
+function checkspeed(speed) {
+  const speedlimit = 70;
+  const kmperpoint = 5;
+
+  if (speed < speedlimit + kmperpoint) {
+    console.log("Ok");
+    return;
+  } else {
+    const points = Math.floor((speed - speedlimit) / kmperpoint);
+    if (points >= 12) console.log("LICENSED SUSPENDED");
+    else console.log("points", points);
+  }
+}
